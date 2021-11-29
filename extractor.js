d8305f690ccedb4732e183f3b6a03535bbab5dd2
Ollie-Boyd <ollyboyd@hotmail.co.uk>
2021-11-29 14:35:51 +0000

    removed consolelogs and moved js to file

---
+++ extractor.js
@@ -0,0 +1,27 @@
+function getPostId() {
+    const linkedinURL= document.querySelector("#url").value;
+    const regex = /([0-9]{19})/;
+    const postId = regex.exec(linkedinURL).pop();
+    return postId;
+  }
+
+  function extractUnixTimestamp(postId) {
+    // BigInt needed as we need to treat postId as 64 bit decimal. This reduces browser support.
+    const asBinary = BigInt(postId).toString(2);
+    const first41Chars = asBinary.slice(0, 41);
+    const timestamp = parseInt(first41Chars, 2);
+    return timestamp;
+  }
+
+  function unixTimestampToHumanDate(timestamp) {
+    const dateObject = new Date(timestamp);
+    const humanDateFormat = dateObject.toUTCString()+" (UTC)";
+    return humanDateFormat;
+  }
+
+  function getDate() {
+    const postId = getPostId();
+    const unixTimestamp = extractUnixTimestamp(postId);
+    const humanDateFormat = unixTimestampToHumanDate(unixTimestamp);
+    document.querySelector("#date").textContent = humanDateFormat;
+  }
